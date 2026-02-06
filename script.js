00ae9b072f9d7466f6b26f525b3acda72f319379
unnatik816-source <unnatik816@gmail.com>
2026-02-06 13:34:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
+<script>
+let musicEnabled = false;
+
+function enableMusic() {
+  const music = document.getElementById("music");
+  music.volume = 0.7;
+
+  music.play()
+    .then(() => {
+      musicEnabled = true;
+      alert("Sound enabled 🎶");
+    })
+    .catch(() => {
+      alert("Please tap again to allow sound 🎵");
+    });
+}
+
+function startSurprise() {
+  document.getElementById("questionBox").classList.remove("hidden");
+
+  if (!musicEnabled) {
+    alert("Please tap 'Enable sound' first 🎵");
+  }
+}
+
 function yesClicked() {
-    document.getElementById("photos").classList.remove("hidden");
-    alert("YAYYYY! 💖 I knew it 😘");
+  document.getElementById("questionBox").classList.add("hidden");
+  document.getElementById("finalBox").classList.remove("hidden");
 }
 
 function noClicked() {
-    alert("There is no such option 😌❤️");
+  alert("There is no such option 😌❤️");
 }
+</script>
